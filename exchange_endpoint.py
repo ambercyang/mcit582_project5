b2de2560374575057739772ee796ac210787ac20
Chao Yang <49256477+ambercyang@users.noreply.github.com>
2021-04-25 20:53:58 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -171,8 +171,8 @@ def fill_order(order, txes=[]):
     # Match orders (same as Exchange Server II)
     #1.Insert the order
     order_obj = Order(sender_pk=order['sender_pk'],receiver_pk=order['receiver_pk'], buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], buy_amount=order['buy_amount'], sell_amount=order['sell_amount'] )
-    session.add(order_obj)
-    session.commit()
+    g.session.add(order_obj)
+    g.session.commit()
 
     #2.Check if there are any existing orders that match
     matched_order = session.query(Order).filter(Order.filled==None,                                       Order.buy_currency == order_obj.sell_currency,                                       Order.sell_currency == order_obj.buy_currency,                                       Order.sell_amount/Order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount).first()
@@ -187,15 +187,15 @@ def fill_order(order, txes=[]):
         if order_obj.buy_amount > matched_order.sell_amount:
                 new_order = Order(sender_pk = order_obj.sender_pk,receiver_pk = order_obj.receiver_pk,                                   buy_currency = order_obj.buy_currency, sell_currency = order_obj.sell_currency,                                   buy_amount = order_obj.buy_amount - matched_order.sell_amount,                                   sell_amount = (order_obj.buy_amount - matched_order.sell_amount)* order_obj.sell_amount / order_obj.buy_amount,                                  creator_id = order_obj.id)
                 #print("partially filled, new_order.buy_amount > matched_order.sell amount, creator_id =", new_order.creator_id)
-                session.add(new_order)
-                session.commit()
+                g.session.add(new_order)
+                g.session.commit()
                   
         if matched_order.buy_amount > order_obj.sell_amount:
                 new_order = Order(sender_pk = matched_order.sender_pk,receiver_pk = matched_order.receiver_pk,                                   buy_currency =matched_order.buy_currency, sell_currency = matched_order.sell_currency,                                   buy_amount = matched_order.buy_amount - order_obj.sell_amount,                                   sell_amount= (matched_order.buy_amount - order_obj.sell_amount) * matched_order.sell_amount / matched_order.buy_amount,                                  creator_id = matched_order.id)
                 #print("partially filled, matched_order.buy_amount>new_order.sell_amount, creator_id =", new_order.creator_id)
                 
-                session.add(new_order)
-                session.commit()
+                g.session.add(new_order)
+                g.session.commit()
                 
     # Validate the order has a payment to back it (make sure the counterparty also made a payment)
     if(order['sell_currency'] == "Ethereum"):
